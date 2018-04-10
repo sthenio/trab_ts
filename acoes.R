f72795e3de2a0520758d7526584ed4bb399f1db0
sthenio <sthenio.reis@hotmail.com>
2018-04-10 19:25:34 -0400

    Add files via upload

---
+++ acoes.R
@@ -0,0 +1,5 @@
+setwd("C:/Users/user/Documents/Economia/Dados/Cotaçoes Históricas/BRFS3")
+
+br_foods_on = read.table("BRFS3.SA.csv",
+                         header = FALSE,
+                         sep = ",")
